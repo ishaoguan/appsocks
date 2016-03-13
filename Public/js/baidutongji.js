5c4e89b721754128e723918c5ccf74be746ec75b
sugarman <voganwong@gmail.com>
2016-03-14 01:59:43 +0800

    增加百度统计。（个人使用需更改baidutongji.js）

---
+++ Public/js/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?32bee89908969fd5e0264c27dea3a4b0";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
