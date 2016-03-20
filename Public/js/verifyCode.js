11360bc44a93d9f8232616a6f9057013ef5afd80
sugarman <voganwong@gmail.com>
2016-03-20 15:16:31 +0800

    1.验证码路径修复

--- Public/js/verifyCode.js
+++ Public/js/verifyCode.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    var validation_path = '/appsocks/Public/verifyCode';
+    var validation_path = '/Public/verifyCode';
     $('.validation-img').on('click', function() {
         $(this).attr('src', validation_path + '/random='+Math.random());
     });
