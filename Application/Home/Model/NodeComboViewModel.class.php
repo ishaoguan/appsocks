ade06a864455b29a9b99021eabb78cafd122c2e0
sugarman <voganwong@gmail.com>
2016-03-21 22:05:37 +0800

    1.修复套餐停用

--- Application/Home/Model/NodeComboViewModel.class.php
+++ Application/Home/Model/NodeComboViewModel.class.php
@@ -3,7 +3,7 @@ namespace Home\Model;
 use Think\Model\ViewModel;
 class NodeComboViewModel extends ViewModel {
 	public $viewFields = array(
-		'Node'=>array('nid', 'name', 'server_ip', 'domain_name', 'method', 'remark', 'status'),
+		'Node'=>array('nid', 'name', 'server_ip', 'domain_name', 'method', 'remark'),
 		'Combo'=>array('cid', 'flow', 'duration', 'cost', 'title', 'remark', 'status', '_on'=>'Node.nid=Combo.nid'),
 	);
 }
